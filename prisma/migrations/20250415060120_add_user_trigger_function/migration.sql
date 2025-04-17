eb326d9f3c446bd3bf160e78142dbfbf9747e775
n-takeshita <naokisyuu@gmail.com>
2025-04-17 19:51:24 +0900

    `npx prisma migrate dev `で以下のエラーが発生するようになってたので対応。（スキーマ差分を検出するためにshadow databaseをローカルで作っているようだが、authスキーマがないと怒られる。）
    ```
    Error: P3006
    
    Migration `20250415060120_add_user_trigger_function` failed to apply cleanly to the shadow database.
    Error code: P1014
    Error:
    The underlying table for model `auth.users` does not exist.
    ```

--- prisma/migrations/20250415060120_add_user_trigger_function/migration.sql
+++ prisma/migrations/20250415060120_add_user_trigger_function/migration.sql
@@ -38,6 +38,16 @@ END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
 -- Create trigger
-CREATE TRIGGER on_auth_user_created
-AFTER INSERT ON auth.users
-FOR EACH ROW EXECUTE FUNCTION public.handle_new_user();
+DO $$
+BEGIN
+  IF EXISTS (
+    SELECT 1
+    FROM information_schema.schemata
+    WHERE schema_name = 'auth'
+  ) THEN
+    CREATE TRIGGER on_auth_user_created
+    AFTER INSERT ON auth.users
+    FOR EACH ROW EXECUTE FUNCTION public.handle_new_user();
+  END IF;
+END;
+$$;
