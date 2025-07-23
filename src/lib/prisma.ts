413ca8ae93c9106336ff6adb351ad089ada84ba6
n-takeshita <naokisyuu@gmail.com>
2025-07-23 10:12:44 +0900

    update: 本番環境ではクエリログを出力しない

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -8,24 +8,10 @@ const globalForPrisma = global as unknown as {
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: [
-      {
-        emit: "stdout",
-        level: "query",
-      },
-      {
-        emit: "stdout",
-        level: "error",
-      },
-      {
-        emit: "stdout",
-        level: "info",
-      },
-      {
-        emit: "stdout",
-        level: "warn",
-      },
-    ],
+    log:
+      process.env.NODE_ENV === "production"
+        ? ["info", "warn", "error"]
+        : ["query", "info", "warn", "error"],
   }).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
